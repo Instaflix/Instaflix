51cf3e09eed2b048c7d5efcd06c0bc48281de9ee
Eberm024 <edgarmbermudez024@gmail.com>
2021-11-11 15:14:17 -0500

    added images, first screen update

---
+++ Instaflix/Instaflix/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Instaflix
+//
+//  Created by Edgar Bermudez on 11/11/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
